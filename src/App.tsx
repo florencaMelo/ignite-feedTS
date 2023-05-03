23b6f5c1101fc1d2a0d2c88fe1ca170b432d661f
Florença de Melo Junqueira <jflor.melo@gmail.com>
2023-05-03 14:10:49 -0300

    subindo alterações

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,12 @@
 import { Header } from "./components/Header";
-import "./global.css";
 import styles from "./App.module.css";
-import { Post } from "./components/Post";
+import { Post, PostType } from "./components/Post";
 import { Sidebar } from "./components/Sidebar";
-import { Post } from './components/Post'
+
+import "./global.css";
 
 // aqui vai fazer com que as propriedades não fiquem estáticas e sim dinâmicas
-const post =[
+const post: PostType[] =[
   {
     id: 1,
     author: {
@@ -51,9 +51,7 @@ export function App() {
             return(
               <Post
                 key={post.id}
-                author={post.author}
-                content={post.content}
-                publishedAt={post.publishedAt}
+                post={post}
               />
             ) 
           })}
--- src/components/Post.tsx
+++ src/components/Post.tsx
@@ -19,6 +19,7 @@ interface Content{
 }
 
 export interface PostType{
+  id: number;
   author: Author
   publishedAt: Date;
   content: Content[];
